29565e2c013dae26a10bec70e409c302988f41fe
shaileshs3184 <47593948+shaileshs3184@users.noreply.github.com>
2021-11-26 19:13:12 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "myawsserver" {
 
   tags = {
     Name = "Gagan-devops-server"
-    env = "production"
+    env = "production123"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /root/inv"
